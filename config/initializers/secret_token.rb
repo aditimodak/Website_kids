e8e9110ca82505bc00f3822289b7f6522c3a2e3a
Aditi Modak <aditi.modak@west.cmu.edu>
2012-09-10 23:45:31 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebsiteKids::Application.config.secret_token = '83e687dca7c92c8363cc5b264fa1e159e4ee20e18736fe08e5ad725d249ee3554edbf52662b9a1c1e7f71e9684f9780068c1b79c447b94ac5b67977ccb7e1770'
